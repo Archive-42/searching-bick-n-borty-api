054e49b7fc5621df24dce81c25f3bdeba0e08d49
bryan <bryan.guner@gmail.com>
2021-11-24 01:36:02 -0500

    initial commit

--- src/widgets/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ClearFiltersMobile } from "./ClearFiltersMobile";
-export { default as NoResults } from "./NoResults";
-export { default as ResultsNumberMobile } from "./ResultsNumberMobile";
-export { default as SaveFiltersMobile } from "./SaveFiltersMobile";
